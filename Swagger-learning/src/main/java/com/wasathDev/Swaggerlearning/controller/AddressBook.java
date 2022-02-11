004a53dbe4216ab1b321cf03c31a509bf722dd49
Theekshana <47950733+wasthTheekshana@users.noreply.github.com>
2022-02-11 12:10:37 +0530

    adding swagger depedcy

--- Swagger-learning/src/main/java/com/wasathDev/Swaggerlearning/controller/AddressBook.java
+++ Swagger-learning/src/main/java/com/wasathDev/Swaggerlearning/controller/AddressBook.java
@@ -1,4 +1,31 @@
 package com.wasathDev.Swaggerlearning.controller;
 
+import com.wasathDev.Swaggerlearning.dto.Contact;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@RestController
+
 public class AddressBook {
+
+    ConcurrentMap<String,Contact> map = new ConcurrentHashMap<>();
+
+    @GetMapping("/{id}")
+    public Contact getContact(@PathVariable String id){
+        return map.get(id);
+    }
+
+    @GetMapping("/")
+    public List<Contact>getAllContact(){
+        return new ArrayList<Contact>(map.values());
+    }
+
+    @PostMapping("/")
+    public Contact addContact(@RequestBody Contact contact){
+        return map.put(contact.getId(), contact);
+    }
 }
---
+++ Swagger-learning/src/main/java/com/wasathDev/Swaggerlearning/dto/Contact.java
@@ -0,0 +1,15 @@
+package com.wasathDev.Swaggerlearning.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Contact {
+
+    private String id;
+    private String name;
+    private String phone;
+
+
+}
