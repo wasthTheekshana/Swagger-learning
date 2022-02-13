2a7209533799a5f02eadca814760a870ea615bfe
Theekshana <47950733+wasthTheekshana@users.noreply.github.com>
2022-02-13 11:31:10 +0530

    part one done

--- Swagger-learning/src/main/java/com/wasathDev/Swaggerlearning/controller/AddressBook.java
+++ Swagger-learning/src/main/java/com/wasathDev/Swaggerlearning/controller/AddressBook.java
@@ -9,7 +9,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 @RestController
-
+@RequestMapping("/api")
 public class AddressBook {
 
     ConcurrentMap<String,Contact> map = new ConcurrentHashMap<>();
